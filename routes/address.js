809e464079011602df1f5372a4d3dfceeec39afb
MJ <blacky512@naver.com>
2012-11-08 21:15:37 -0800

    fini

--- routes/address.js
+++ routes/address.js
@@ -1,62 +1,72 @@
 module.exports = function(app){
 	
 	app.get('/', function(req, res){
-		res.render('mypage', {title:'index'});
+		res.render('mypage/mypage', {title:'index'});
 	});
 
 	// mypage 카테고리 라우팅
 	app.get('/mypage/mypage', function(req, res){
-		res.render('mypage', {title:'/my/mypage'});
+		res.render('mypage/mypage', {title:'/my/mypage'});
 	});
 	app.get('/mypage/bookmark', function(req, res){
-		res.render('bookmark', {title:'/my/bookmark'});
+		res.render('mypage/bookmark', {title:'/my/bookmark'});
 	});
 	app.get('/mypage/my_nailo', function(req, res){
-		res.render('my_nailo', {title:'/my/my_nailo'});
+		res.render('mypage/my_nailo', {title:'/my/my_nailo'});
 	});
 	app.get('/mypage/my_community', function(req, res){
-		res.render('my_community', {title:'/my/my_community'});
+		res.render('mypage/my_community', {title:'/my/my_community'});
 	});
 	app.get('/mypage/share', function(req, res){
-		res.render('share', {title:'/my/share'});
+		res.render('mypage/share', {title:'/my/share'});
 	});
 	app.get('/mypage/login', function(req, res){
-		res.render('login', {title:'/my/login'});
+		res.render('mypage/login', {title:'/my/login'});
 	});
 	app.get('/mypage/phone_auth', function(req, res){
-		res.render('phone_auth', {title:'/my/phone_auth'});
+		res.render('mypage/phone_auth', {title:'/my/phone_auth'});
 	});
 
 
 	// community 카테고리 라우팅
 	app.get('/community/community', function(req, res){
-		res.render('community', {title:'/community/community'});
+		res.render('community/community', {title:'/community/community'});
 	});	
 	app.get('/community/bun_make', function(req, res){
-		res.render('bun_make', {title:'/community/bun_make'});
+		res.render('community/bun_make', {title:'/community/bun_make'});
 	});	
 	app.get('/community/bun_show', function(req, res){
-		res.render('bun_show', {title:'/community/bun_show'});
+		res.render('community/bun_show', {title:'/community/bun_show'});
 	});	
 	
 	
 	// plan 카테고리 라우팅
 	app.get('/plan/plan', function(req, res){
-		res.render('plan', {title:'/plan/plan'});
+		res.render('plan/plan', {title:'/plan/plan'});
 	});	
+	// 이후 plan 1~3 합체
+	app.get('/plan/plan2', function(req, res){
+		res.render('plan/plan2', {title:'/plan/plan2'});
+	});	
+	app.get('/plan/plan3', function(req, res){
+		res.render('plan/plan3', {title:'/plan/plan3'});
+	});	
+	
+
 	app.get('/plan/see', function(req, res){
-		res.render('see', {title:'/plan/see'});
+		res.render('plan/see', {title:'/plan/see'});
 	});	
 	
 	
 	// etc 카테고리 라우팅
 	app.get('/etc/notice', function(req, res){
-		res.render('notice', {title:'/etc/notice'});
+		res.render('etc/notice', {title:'/etc/notice'});
 	});	
 	app.get('/etc/setting', function(req, res){
-		res.render('setting', {title:'/etc/setting'});
+		res.render('etc/setting', {title:'/etc/setting'});
 	});	
-	app.get('/etc/notice', function(req, res){
-		res.render('tip', {title:'/etc/tip'});
+	app.get('/etc/tip', function(req, res){
+		res.render('etc/tip', {title:'/etc/tip'});
 	});	
 	
+}
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,5 @@
-
 /*
- * GET home page.
- */
-
-<<<<<<< HEAD
-=======
-exports.index = function(req, res){
-  res.render('myPage', { title: 'IlowaNailo' });
+ exports.index = function(req, res){
+  res.render('/mypage/myPage', { title: 'mypage.jade' });
 };
-
-exports.share = function(req,res){
-	res.render('share',{title:'IlowaNailo'});
-};
-
-exports.book = function(req,res){
-	res.render('bookmark',{title:'IlowaNailo'});
-};
-
-exports.my = function(req, res){
-	res.render('mynailo',{title:'IlowaNailo'});
-};
-
-exports.plan = function(req,res){
-	res.render('plan', { title: 'Plan' });
-};
-
-exports.plan2 = function(req,res){
-	res.render('plan2', { title: 'Plan2' });
-};
->>>>>>> 90fc0f29b16ca43ae48a386e1508fde0b7426722
+*/ 
