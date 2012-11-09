8403c02e7d479584779b11dbc3b3085e4fca2c2f
righ120 <er2357@naver.com>
2012-11-09 19:17:31 +0900

    슬라이드 등등

--- routes/address.js
+++ routes/address.js
@@ -1,10 +1,10 @@
 module.exports = function(app){
 
 	app.get('/', function(req, res){
-	  res.render('myPage', { title: 'IlowaNailo' });
+	  res.render('mypage', { title: 'IlowaNailo' });
 	});
 	
-	app.get('/share', function(req,res){
+	app.get('/mypage/share', function(req,res){
 		res.render('share',{title:'IlowaNailo'});
 	});
 	
@@ -16,8 +16,39 @@ module.exports = function(app){
 		res.render('mynailo',{title:'IlowaNailo'});
 	});
 	
-	app.get('/mypage', function(req, res){
-		res.render('mynailo',{title:'IlowaNailo'});
+	app.get('/mypage/my_community', function(req, res){
+		res.render('my_community',{title:'IlowaNailo'});
+	});
+	
+	app.get('/community', function(req, res){
+		res.render('community',{title:'IlowaNailo'});
+	});
+	
+	app.get('/community#community_2', function(req, res){
+		res.render('/community#community_2',{title:'IlowaNailo'});
+	});
+	
+	app.get('/community#community_3', function(req, res){
+		res.render('/community#community_3',{title:'IlowaNailo'});
+	
+	app.get('/community/bun_make', function(req, res){
+		res.render('bun_make',{title:'IlowaNailo'});
+	});
+	
+	app.get('/mypage/bun_show', function(req, res){
+		res.render('bun_show',{title:'IlowaNailo'});
+	});
+	
+	app.get('/etc/notice', function(req, res){
+	  res.render('notice', { title: 'IlowaNailo' });
+	});
+	
+	app.get('/etc/setting', function(req, res){
+	  res.render('setting', { title: 'IlowaNailo' });
+	});
+	
+	app.get('/etc/tip', function(req, res){
+	  res.render('tip', { title: 'IlowaNailo' });
 	});
 	
 	app.get('/plan', function(req,res){
