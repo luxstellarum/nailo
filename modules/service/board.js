001336a7c03548bf5929c80c50f4a92755bcfa8f
JunHong <far_cry@naver.com>
2012-11-17 01:52:39 +0900

    test'

--- modules/service/board.js
+++ modules/service/board.js
@@ -67,7 +67,6 @@ module.exports = {
 		board_db.get(condition, function(result){
 			if(result != false) {
 				console.log('service/board.js, view success');
-				result[result] = true;
 				res.json(result);
 			}//end of if
 			else {
