1eec997e69ce66421ba0c7945e32a2923b453bc3
righ120 <er2357@naver.com>
2012-11-22 01:33:45 +0900

    dpr

--- public/javascripts/mypage/my_community.js
+++ public/javascripts/mypage/my_community.js
@@ -1,8 +1,26 @@
 $(document).unbind().bind('pagecreate',function(){
 	
+	var output_data={};
+	
 	$.ajax({
 		type:'post',
-		url:'/board/list',
-		data: ''
+		dataType: 'json',
+		url:'/member/get_login_inform',
+		data: output_data,
+		success:function(data){
+			if(data.user_id){
+				console.log(output_data['usr_id']);
+				$.ajax({
+					type: 'post',
+					dataType: 'json',
+					url:'/board/list',
+					data:output_data
+				});				
+			}
+			else
+			{
+				alert('fail');
+			}
+		}
 	});
 });
