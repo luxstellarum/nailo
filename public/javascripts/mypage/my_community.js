dde402daf3ebc4414298c347bbe2f20c079505fa
righ120 <er2357@naver.com>
2012-11-22 09:40:48 +0900

    dpr

--- public/javascripts/mypage/my_community.js
+++ public/javascripts/mypage/my_community.js
@@ -17,7 +17,20 @@ $(document).unbind().bind('pagecreate',function(){
 					data:output_data,
 					success:function(data){
 						if(data.result != false){
-								alert('success');
+								$.each(data,function(i,item){
+									var div = document.createElement('div');
+									div.innerHTML = document.getElementById('pre_set').innerHTML;
+									div.style.marginTop="0px";
+									div.setAttribute("name",item.index);
+									div.firstChild.style.overflow="hidden";
+									item.start_hour=SetZeros(item.start_hour,2);
+									item.start_minute=SetZeros(item.start_minute,2);									
+									div.firstChild.appendChild(
+										document.createTextNode(" ["+item.city+"/"+item.event_month+"."+item.event_date+"/"
+																+item.start_hour+":"+item.start_minute+"] "+item.content));
+									
+									$('#field').append(div);
+								});
 							}
 							else{
 								alert('fail');
@@ -32,3 +45,13 @@ $(document).unbind().bind('pagecreate',function(){
 		}
 	});
 });
+
+function SetZeros(num, digits) {
+	var Zeros = '';
+	num = num.toString();
+	if (num.length < digits) {
+		for (i = 0; i < digits - num.length; i++)
+		Zeros += '0';
+	}
+	return Zeros + num;
+}
