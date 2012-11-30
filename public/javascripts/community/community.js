6f29cb0103b5dd8cf080b44b85a86b62ce0dd8b3
JunHong <far_cry@naver.com>
2012-11-30 18:48:01 +0900

    ddd

--- public/javascripts/community/community.js
+++ public/javascripts/community/community.js
@@ -1,6 +1,6 @@
 
 //page 로딩이 끝난 후에 각종 이벤트 및 함수를 로딩 
-$(document).unbind().bind('pagecreate',function(){
+$(document).bind('pagecreate',function(){
 	// section 높이를 구한다
 	var window_height = $(window).height();
 	var section_height = window_height - 140 - 60;	// section 높이는 header, footer을 뺸 나머지
